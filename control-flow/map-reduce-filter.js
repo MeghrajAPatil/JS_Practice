05c4f3f7bfe4c9d785eb7a3e344c1b411c6f5b1b
MeghrajAPatil <164365502+MeghrajAPatil@users.noreply.github.com>
2026-02-04 21:24:43 +0000

    container config done

--- control-flow/map-reduce-filter.js
+++ control-flow/map-reduce-filter.js
@@ -8,3 +8,20 @@ console.log(b);
 let c = arr.map((curele)=> curele*curele)
 console.log(c);
 
+let lib = [
+    {
+        course : "js",
+        price : 10000
+    },
+        {
+        course : "py",
+        price : 20000
+    },
+        {
+        course : "java",
+        price : 15000
+    },
+]
+
+const add = lib.reduce((accum,item)=>accum+item.price,0)
+console.log(add);
